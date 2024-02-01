e563c94339d10a9a24680cba6f8d78e99864d743
Eda Sampad <139133880+Eda-Sampad-052@users.noreply.github.com>
2024-02-01 19:36:37 +0530

    Update 12c.c

--- week_12/12c.c
+++ week_12/12c.c
@@ -1,28 +1,27 @@
 #include <stdio.h>
 void main()
 {
-    //to count and display the number of character,words,and lines in a file 12c.txt
-    FILE *fp;
-    fp=fopen("12c.txt","r");
-    int c=0,w=0,l=0;
-    char ch;
-    while((ch=fgetc(fp))!=EOF)
-    {
-        if(ch=='\n')
-        {
-            l++;
-        }
-        else if(ch==' ')
-        {
-            w++;
-        }
-        else
-        {
-            c++;
-        }
-    }
-    fclose(fp);
-    printf("Number of characters in the file is %d\n",c);
-    printf("Number of words in the file is %d\n",w);
-    printf("Number of lines in the file is %d\n",l);
+FILE *fp;
+fp=fopen("12c.txt","r");
+int c=0,w=0,l=0;
+char ch;
+while((ch=fgetc(fp))!=EOF)
+{
+if(ch=='\n')
+{
+l++;
+}
+else if(ch==' ')
+{
+w++;
+}
+else
+{
+c++;
+}
+}
+fclose(fp);
+printf("Number of characters in the file is %d\n",c);
+printf("Number of words in the file is %d\n",w);
+printf("Number of lines in the file is %d\n",l);
 }
