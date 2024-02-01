44717ee77c09b234eea4d34ed9c325be14652ee7
Eda Sampad <139133880+Eda-Sampad-052@users.noreply.github.com>
2024-02-01 19:38:27 +0530

    Update 12d.c

--- week_12/12d.c
+++ week_12/12d.c
@@ -1,30 +1,22 @@
 #include <stdio.h>
 
 void main() {
-    int n, i;
-    char a[100], b[100];
-
-    printf("Enter the file name: ");
-    scanf("%s", a);
-
-    printf("Enter the number of characters: ");
-    scanf("%d", &n);
-
-    FILE *fp;
-    fp = fopen(a, "r");
-
-    if (fp == NULL) {
-        printf("Error opening file.\n");
-        return;
-    }
-
-    fseek(fp, -n, SEEK_END);
-
-    for (i = 0; i < n; i++) {
-        fscanf(fp, "%c", &b[i]);
-    }
-
-    printf("Last %d characters: %s\n", n, b);
-
-    fclose(fp);
+int n, i;
+char a[100], b[100];
+printf("Enter the file name: ");
+scanf("%s", a);
+printf("Enter the number of characters: ");
+scanf("%d", &n);
+FILE *fp;
+fp = fopen(a, "r");
+if (fp == NULL) {
+printf("Error opening file.\n");
+return;
+}
+fseek(fp, -n, SEEK_END);
+for (i = 0; i < n; i++) {
+fscanf(fp, "%c", &b[i]);
+}
+printf("Last %d characters: %s\n", n, b);
+fclose(fp);
 }
