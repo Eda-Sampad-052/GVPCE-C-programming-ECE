c2ff790371691482b4eddbac71e0006ba252eeb9
Eda Sampad <139133880+Eda-Sampad-052@users.noreply.github.com>
2024-02-01 18:59:15 +0530

    Add files via upload

---
+++ week_12/12a.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void main()
+{
+    FILE *f1;
+    f1=fopen("12a.txt","r");
+    char c;
+    while((c=fgetc(f1))!=EOF)
+    {
+        printf("%c",c);
+    }
+    fclose(f1);
+
+}
---
+++ week_12/12b.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+void main()
+{
+
+    //to copy content of 12a.txt to 12b.txt and print 12b.txt
+    FILE *fp1, *fp2;
+    fp1 = fopen("12a.txt", "r");
+    fp2 = fopen("12b.txt", "w");
+    char ch;
+    while ((ch = fgetc(fp1))!= EOF)
+        fputc(ch, fp2);
+    fclose(fp1);
+    fclose(fp2);
+    fp1 = fopen("12b.txt", "r");
+    while ((ch = fgetc(fp1))!= EOF)
+        printf("%c", ch);
+    fclose(fp1);
+
+    }
