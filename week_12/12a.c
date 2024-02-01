b668da28f5dcb0505f6ce93d55025cb36667bdc9
Eda Sampad <139133880+Eda-Sampad-052@users.noreply.github.com>
2024-02-01 19:25:23 +0530

    Update 12a.c

--- week_12/12a.c
+++ week_12/12a.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 void main()
 {
-    FILE *f1;
-    f1=fopen("12a.txt","r");
-    char c;
-    while((c=fgetc(f1))!=EOF)
-    {
-        printf("%c",c);
-    }
-    fclose(f1);
-
+FILE *f1;
+f1=fopen("12a.txt","r");
+char c;
+while((c=fgetc(f1))!=EOF)
+{
+printf("%c",c);
+}
+fclose(f1);
 }
