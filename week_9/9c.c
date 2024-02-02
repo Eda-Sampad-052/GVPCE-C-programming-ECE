8c04ade87c46214596e8257a2c94ad5bc9b5d32c
Eda Sampad <139133880+Eda-Sampad-052@users.noreply.github.com>
2024-02-02 16:34:36 +0530

    Update 9c.c

--- week_9/9c.c
+++ week_9/9c.c
@@ -6,23 +6,18 @@ void main() {
     int *ptr = &i;
     printf("\nAddress of the variable: %p\n", (void*)&i);
     printf("Value of the variable: %d\n", i);
-    printf("\nPointer Addition:\n");
     ptr = ptr + 1;
     printf("Address after pointer addition: %p\n", (void*)ptr);
     printf("Value of the variable: %d\n", *ptr);
-    printf("\nPointer Subtraction:\n");
     ptr = ptr - 1;
     printf("Address after pointer subtraction: %p\n", (void*)ptr);
     printf("Value of the variable: %d\n", *ptr);
-    printf("\nPointer Increment:\n");
     ptr++;
     printf("Address after pointer increment: %p\n", (void*)ptr);
     printf("Value of the variable: %d\n", *ptr);
-    printf("\nPointer Decrement:\n");
     ptr--;
     printf("Address after pointer decrement: %p\n", (void*)ptr);
     printf("Value of the variable: %d\n", *ptr);
-    printf("\nPointer Comparison:\n");
     int* ptr2 = &i;
     if (ptr == ptr2) {
         printf("Pointers are equal.\n");
